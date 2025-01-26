7f0738ff92e64e4547f01187f3a70143137e24fb
Pavel Skrylev <majioa@altlinux.org>
2025-01-26 18:19:02 +0300

    fix: updated license map list
    ! updated license map list

--- lib/baltix/license.rb
+++ lib/baltix/license.rb
@@ -1,6 +1,12 @@
 module Baltix::License
    LIST = {
-      "AFL" => /AFL/
+      "AFL-1.2" => /AFL-1\.1/,
+      "AFL-1.2" => /AFL-1\.2/,
+      "AFL-1.2" => /AFL-2\.0/,
+      "AFL-1.2" => /AFL-2\.1/,
+      "AFL-1.2" => /AFL-3\.0/,
+      "AFL-3.0" => /AFL/,
+      "Apache-2.0" => /Apache[\-\s]2(\.0|).*/,
    }
    class << self
       def parse license
